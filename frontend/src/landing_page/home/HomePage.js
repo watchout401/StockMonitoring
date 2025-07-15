735d2604131cab77ecc25d282412f0d2df6b0424
watchout401 <abhishek.kvisha@gmail.com>
2025-07-16 00:23:26 +0530

    Initial commit of Stock Monitoring Tool project

---
+++ frontend/src/landing_page/home/HomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
+
+import OpenAccount from "../OpenAccount";
+
+import Footer from "../Footer";
+
+function HomePage() {
+  return (
+    <>
+      
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
